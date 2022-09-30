fb4e3dc6a905d6ba178a99cb861acbfbd0d34c26
andrevpc <111889239+andrevpc@users.noreply.github.com>
2022-09-29 21:42:05 -0300

    Add files via upload

---
+++ Lista 6.cpp
@@ -0,0 +1,114 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <math.h>
+
+////Exercício 1 - for
+//int main(){
+//	setlocale(LC_ALL,"Portuguese");
+//	int I;
+//	for(I=1;I<=10;I++)
+//		printf("Número: %d.\n", I);
+//	system("pause");
+//	return(0);
+//}
+
+////Exercício 1 - while
+//int main(){
+//	setlocale(LC_ALL,"Portuguese");
+//	int I;
+//	I=1;
+//	while (I<=10){
+//		printf("Número: %d.\n", I);
+//		I++;
+//}
+//	system("pause");
+//	return(0);
+//}
+
+////Exercício 1 - do while
+//int main(){
+//	setlocale(LC_ALL,"Portuguese");
+//	int I;
+//	I = 1;
+//	do{
+//		printf("Número: %i.\n", I);
+//		I = I + 1;
+//	}while(I<=10);
+//	system("pause");
+//	return(0);
+//}
+
+////Exercício 2
+//int main(){
+//	setlocale(LC_ALL,"Portuguese");
+//	int I;
+//	for(I=10;I>=0;I--)
+//		printf("Número: %d.\n", I);
+//	system("pause");
+//	return(0);
+//}
+
+////Exercício 3
+//int main(){
+//	setlocale(LC_ALL,"Portuguese");
+//	int I;
+//	for(I=100;I<=110;I++)
+//		printf("Número: %d.\n", I);
+//	system("pause");
+//	return(0);
+//}
+
+////Exercício 4
+//int main(){
+//	setlocale(LC_ALL,"Portuguese");
+//	int I, N;
+//	printf("Escolha o N: ");
+//	scanf("%d", &N);
+//	for(I=1;I<=N;I++)
+//		printf("Número: %d.\n", I);
+//	system("pause");
+//	return(0);
+//}
+
+////Exercício 5
+//int main(){
+//	setlocale(LC_ALL,"Portuguese");
+//	int I, N, aux;
+//	aux = 1;
+//	while (aux == 1){
+//		printf("Escolha o N: ");
+//		scanf("%d", &N);
+//		if (N > 0){
+//			for(I=1;I<=N;I++)
+//			printf("Número: %d.\n", I);
+//		aux = 0;
+//		}
+//		}
+//	
+//	system("pause");
+//	return(0);
+//}
+
+////Exercício 6
+//int main(){
+//	setlocale(LC_ALL,"Portuguese");
+//	int I;
+//	for(I=1;I<=10;I++)
+//		printf("8X %d = %d.\n", I, I*8);
+//	system("pause");
+//	return(0);
+//}
+
+////Exercício 7
+//int main(){
+//	setlocale(LC_ALL,"Portuguese");
+//	int I, N;
+//	printf("Escolha o N: ");
+//	scanf("%d", &N);
+//	for(I=1;I<=10;I++)
+//		printf("%d X %d = %d.\n",N, I, N*I);
+//	system("pause");
+//	return(0);
+//}
